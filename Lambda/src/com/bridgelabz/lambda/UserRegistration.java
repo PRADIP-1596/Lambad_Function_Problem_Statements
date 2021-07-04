bd04863ad6f434d25074691e27370622b09b4715
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-07-04 22:43:37 +0530

    "Commit Email Validation In Lambda Expressions As Pushed"

--- Lambda/src/com/bridgelabz/lambda/UserRegistration.java
+++ Lambda/src/com/bridgelabz/lambda/UserRegistration.java
@@ -5,31 +5,31 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @FunctionalInterface
-interface FirstName {
-  void firstNameValidate();
+interface LastName{
+    void lastNameValidate();
 }
 
 
 public class UserRegistration {
-  public static void main(String[] args ){
-      //Lambda -> operator uses
-      FirstName fName = () -> {
-          Scanner sc = new Scanner(System.in);
+    public static void main(String[] args ){
+        //Lambda -> operator uses
+        LastName lName = () -> {
+            Scanner sc = new Scanner(System.in);
 
-          String regex = "^[A-Z]{1}[a-z]{3,20}$";
+            String regex = "^[A-Z]{1}[a-z]{3,20}$";
 
-          System.out.println("Enter your name");
-          String name = sc.next();
+            System.out.println("Enter your last name");
+            String name = sc.next();
 
-          Pattern pattern = Pattern.compile(regex);
-          Matcher matcher = pattern.matcher(name);
-          if(matcher.matches()){
-              System.out.println("Given name id is valid");
-          }else{
-              System.out.println("Given name id is not valid");
-          }
-      };
-      fName.firstNameValidate();
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(name);
+            if(matcher.matches()){
+                System.out.println("Given last name id is valid");
+            }else{
+                System.out.println("Given last name id is not valid");
+            }
+        };
+        lName.lastNameValidate();
 
-  }
+    }
 }
